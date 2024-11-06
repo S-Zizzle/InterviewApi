d420c73a16881ef73657f309a114c3bf79cea94f
John Birch <john.luke.birch@gmail.com>
2024-11-06 17:19:38 +0500

    updated readme

--- Migrations/20241106121432_testMigration.Designer.cs
+++ Migrations/20241106121432_testMigration.Designer.cs
@@ -2,6 +2,7 @@
 using InterviewApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InterviewApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241106121432_testMigration")]
+    partial class testMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 To run this demo:
 
+# Start database
+Install postgreSQL.
+Add and migrate model. Run from root of cloned dir.
+
+`dotnet ef migrations add InitialCreate`
+
+`dotnet ef database update`
+
 # Start API
 Install .NET Core 8.0(.10) and SDK.
-Open root diretory and run
-`dotnet run`
-Open the link shown in the output (like http://localhost:XXXX)
 
-# Start database
-x
+Open root diretory and run
+
+`dotnet run`
+
+# Make requests
+## Swagger
+Open the link shown in the output (like http://localhost:XXXX)
+## Postman
+tjtjtjt
 
 # Endpoint docs
 ### GET /Products
